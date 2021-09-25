a70b210f4b0b78823404599b5843d23f98a12715
ArsenAjiev <selftcoder@gmail.com>
2021-09-25 13:09:30 +0300

    add files

--- the_weather/the_weather/settings.py
+++ the_weather/the_weather/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8d7es-&td2n5u3r2^o%s3+++0gs3^!th8)=^q-pm0z_66-!_u2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['still-cliffs-76653.herokuapp.com', '127.0.0.1']
 
